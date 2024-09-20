c3c0b3917af835fbed346b2fcd55ab34cce99675
Daniela Bryon <daniellabryren@icloud.com>
2024-09-20 15:49:53 +0000

    traffic light finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+
+
+
+const TrafficLight = () => {
+	const [ currentColor, setCurrentColor] = useState("red");
+	const [ colors, setColors] = useState(["red", "yellow", "green",]);
+	const [ intervalId, setIntervalId] = useState(null);
+	const [ purpleAdded, setPurpleAdded] = useState (false);
+	const [ isChanging, setIsChanging] = useState(false);
+
+
+    
+    //función que cambia el color actual manualmente
+	const changeColor = (color) => {
+		setCurrentColor(color);
+	};
+
+	//color violeta
+	const addPurple = () => {
+		setColors((prev) => purpleAdded ? prev.filter(c => c !== "purple") : [...prev, "purple"]);
+		setPurpleAdded(!purpleAdded);
+    
+	// cambiar color automáticamente
+	const startAutoChange = () => {
+		if (!intervalId) {   // inicia si no hay un intervalo en curso
+			const id = setInterval (() => {
+				const randomColor = colors[Math.floor(Math.random() * colors.length)];
+				setCurrentColor(randomColor);
+			}, 1000); // cambia cada segundo
+			setIntervalId(id);
+			setIsChanging(true);
+		} else {
+			clearInterval(intervalId);
+			setIntervalId(null);
+			setIsChanging(false);
+		}
+	};
+	
+	// limpiar el intervalo al desmontar el componente
+	useEffect (() => () => clearInterval(intervalId), [intervalId]);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className = "container">
+			<h1>Traffic Light</h1>
+			<div className="traffic-lights">
+				{colors.map((color) => (
+					<span
+					    key = {color}
+					    className = {`circle-lights ${color} ${currentColor === color ? "glow" : ""}`}
+					    onClick = {() => changeColor(color)}
+					></span>
+				))}
+			</div>
+			<div className="base"></div>
+			<button onClick={addPurple}>
+				{purpleAdded ? "Delete purple" : "Add purple"}   
+			</button>
+			<button onClick={startAutoChange}>
+				{isChanging ? "Stop change" : "Start change"}
+			</button>
 		</div>
 	);
-};
+};		
 
-export default Home;
+
+export default TrafficLight;
+ReactDOM.render(<TrafficLight />, document.querySelector('#app'));
